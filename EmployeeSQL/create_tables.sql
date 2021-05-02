88a17487eb36e5ec17bf248ea379b3f2f903cd16
Kelsey Vermeer <kelseyvrmr@yahoo.com>
2021-05-02 19:53:38 -0400

    update table schema

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -8,34 +8,36 @@
 --employees and departments have primary keys/are connectors
 
 CREATE TABLE employees(
-	emp_no INT PRIMARY KEY ,
-	emp_title_id VARCHAR(255),
+	emp_no VARCHAR NULL,
+	emp_title_id VARCHAR(255) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
 	sex VARCHAR(255) NOT NULL,
-	hire_date DATE NOT NULL);
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no));
 	
 CREATE TABLE departments(
-	dept_no VARCHAR(255) PRIMARY KEY, 
-	dept_name VARCHAR(255) NOT NULL);
+	dept_no VARCHAR(255) ,
+	dept_name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (dept_no));
 	
 CREATE TABLE dept_emp(
-		emp_no INT NOT NULL,
-		dep_no VARCHAR(255) NOT NULL,
-	foreign key (emp_no) references employees (emp_no),
-	foreign key (dep_no) references departments (dept_no));
+	emp_no VARCHAR NULL,
+	dept_no VARCHAR(255),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
 
 CREATE TABLE dept_manager(
-		dept_no VARCHAR(255) NOT NULL,
-		emp_no INT NOT NULL,
-	foreign key (emp_no) references employees (emp_no),
-	foreign key (dept_no) references departments (dept_no)); 
+	dept_no VARCHAR(255),
+	emp_no VARCHAR NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)); 
 
 CREATE TABLE salaries(
-		emp_no INT NOT NULL, 
-		salary INT NOT NULL,
-	foreign key (emp_no) references employees (emp_no));
+	emp_no VARCHAR NULL, 
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
 CREATE TABLE titles(
 	tile_id VARCHAR(255) NOT NULL,
