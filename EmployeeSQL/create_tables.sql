0952a351776ebae9e53c75033982925ce285dc7f
Kelsey Vermeer <kelseyvrmr@yahoo.com>
2021-05-16 16:04:42 -0400

    updated table creation doc

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,14 +1,14 @@
--- DROP TABLE departments CASCADE; 
--- DROP TABLE dept_emp;
--- DROP TABLE dept_manager;
--- DROP TABLE employees CASCADE;
--- DROP TABLE salaries;
--- DROP TABLE titles; 
+DROP TABLE departments CASCADE; 
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE employees CASCADE;
+DROP TABLE salaries;
+DROP TABLE titles; 
 
 --employees and departments have primary keys/are connectors
 
 CREATE TABLE employees(
-	emp_no VARCHAR NULL,
+	emp_no INT, 
 	emp_title_id VARCHAR(255) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(255) NOT NULL,
@@ -23,19 +23,19 @@ CREATE TABLE departments(
 	PRIMARY KEY (dept_no));
 	
 CREATE TABLE dept_emp(
-	emp_no VARCHAR NULL,
+	emp_no INT NULL,
 	dept_no VARCHAR(255),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(255),
-	emp_no VARCHAR NULL,
+	emp_no INT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)); 
 
 CREATE TABLE salaries(
-	emp_no VARCHAR NULL, 
+	emp_no INT NULL, 
 	salary INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
